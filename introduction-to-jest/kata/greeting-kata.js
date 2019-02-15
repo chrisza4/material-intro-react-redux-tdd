ee2a3478024cb8c8e21cd3d384c7af41226e3646
พชร  ใจสะอาด <pachara.ja@hengleasing.local>
2019-02-15 12:15:36 +0700

    add test case 4, 5, 6

--- introduction-to-jest/kata/greeting-kata.js
+++ introduction-to-jest/kata/greeting-kata.js
@@ -1,14 +1,22 @@
 function greeting (person) {
+  if(isPersons(person)) {
+    let upperCaseName = person.filter(p =>isUpperCase(p))
+    let lowerCaseName = person.filter(p =>!isUpperCase(p))
+
+    let string = "Hello, " + lowerCaseName.slice(0,lowerCaseName.length-1).join(", ").concat(" and ",lowerCaseName[lowerCaseName.length-1], ".")
+
+    if(upperCaseName.length>0)
+      string += " AND HELLO " + upperCaseName.slice(0,upperCaseName.length-1).join(", ").concat(upperCaseName[upperCaseName.length-1],"!")
+
+    return `${string}`
+  }
+
   if (isUpperCase(person)) {
     return `HELLO ${person}!`
   }
+
   const name = person || 'my friend'
   return `Hello, ${name}`
-}
-
-function greeting (persons) {
-  if(isPersons(persons))
-    return "555"
 }
 
 function isUpperCase (word) {
@@ -16,9 +24,12 @@ function isUpperCase (word) {
   return word.toUpperCase() === word
 }
 
-function isPersons(persons) {
-  if(!persons) return false
-  return persons.length > 0
+isPersons = (persons) => {
+  return isNull(persons) && Array.isArray(persons) && persons.length > 0
+}
+
+isNull = (word) => {  
+  return word === null ? false : true
 }
 
 module.exports = {
--- introduction-to-jest/kata/greeting-kata.test.js
+++ introduction-to-jest/kata/greeting-kata.test.js
@@ -11,4 +11,13 @@ describe('greeting', () => {
       should greeting person with shouting tone`, () => {
     expect(greeting('CHRIS')).toEqual('HELLO CHRIS!')
   })
+  it(`test4 `, () => {
+    expect(greeting(["Jill", "Jane"])).toEqual("Hello, Jill and Jane.")
+  })
+  it(`test5 `, () => {
+    expect(greeting(["Amy", "Brian", "Charlotte"])).toEqual("Hello, Amy, Brian and Charlotte.")
+  })
+  it(`test6 `, () => {
+    expect(greeting(["Amy", "BRIAN", "Charlotte"])).toEqual("Hello, Amy and Charlotte. AND HELLO BRIAN!")
+  })
 })
