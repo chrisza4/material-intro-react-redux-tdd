5119da858cf93851814cdc9c5f28096f981547ce
arut klaewkasetkorn <60090002@kmitl.ac.th>
2019-02-15 12:20:39 +0700

    finished requirement 1-8

--- introduction-to-jest/kata/greeting-kata.js
+++ introduction-to-jest/kata/greeting-kata.js
@@ -1,4 +1,17 @@
 function greeting (person) {
+  if(Array.isArray(person)) {
+    person = arrayUtil(person)
+    if(person.length === 2) {
+      return properPrint(person)
+    } else if(person.length === 3 && person.filter((name) => isUpperCase(name)).length === 0) {
+      return `Hello, ${person[0]}, ${person[1]} and ${person[2]}`
+    } else if(person.length === 3 && (person.filter(name => isUpperCase(name)).length !== 0)) {
+      return properPrint(person.filter(name => !isUpperCase(name))) 
+        + '. AND ' 
+        + properPrint(person.filter(name => isUpperCase(name))).toUpperCase()
+    }
+  }
+
   if (isUpperCase(person)) {
     return `HELLO ${person}!`
   }
@@ -9,6 +22,31 @@ function greeting (person) {
 function isUpperCase (word) {
   if (!word) return false
   return word.toUpperCase() === word
+}
+
+
+function properPrint(names) {
+  if(names.length === 1) {
+    return `Hello ${names}`
+  }
+  else if(names.length === 2) {
+    return `Hello, ${names[0]} and ${names[1]}`
+  } else {
+    return `Hello${names
+      .slice(names.length - 1)
+      .map(name => ', ' + name)
+    } and ${name[name.length - 1]}`
+  }
+}
+
+function arrayUtil(names) {
+  return names
+    .map(name => name.split(','))
+    .reduce((a, b) => a.concat(b))
+    .map(name => name.split('\"'))
+    .reduce((a, b) => a.concat(b))
+    .map(name => name.trim())
+    .filter(name => name !== '')
 }
 
 module.exports = {
--- introduction-to-jest/kata/greeting-kata.test.js
+++ introduction-to-jest/kata/greeting-kata.test.js
@@ -11,4 +11,32 @@ describe('greeting', () => {
       should greeting person with shouting tone`, () => {
     expect(greeting('CHRIS')).toEqual('HELLO CHRIS!')
   })
+
+  it(`given a`, () => {
+    expect(greeting('a')).toEqual('Hello, a')
+  })
+
+  it('given two names', () => {
+    expect(greeting(['Jill', 'Jane'])).toEqual('Hello, Jill and Jane')
+  })
+
+  it('given three names', () => {
+    expect(greeting(['Jill', 'Jane', 'John'])).toEqual('Hello, Jill, Jane and John');
+  })
+
+  it('given two lower-cased names and one upper-cased name', () => {
+    expect(greeting(['Jill', 'Jane', 'BRIAN'])).toEqual('Hello, Jill and Jane. AND HELLO BRIAN')
+  })
+
+  it('given one lower-cased name and two upper-cased names', () => {
+    expect(greeting(['Jill', 'JAMES', 'BRIAN'])).toEqual('Hello Jill. AND HELLO, JAMES AND BRIAN')
+  })
+
+  it('given one lower-cased name and two names concatinated in one string', () => {
+    expect(greeting(['Jill', 'James, Brian'])).toEqual('Hello, Jill, James and Brian')
+  })
+
+  it('given one lower-cased name and one CSV manner string', () => {
+    expect(greeting(['Jill', '\"James, Brian\"'])).toEqual('Hello, Jill, James and Brian')
+  })
 })
