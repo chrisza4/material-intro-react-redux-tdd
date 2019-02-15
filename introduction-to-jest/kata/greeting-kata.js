06ed3ee8af76a60b3dd6b43b466bf2fa3a9aa08d
Attapon <attapon@chameleonstrategies.com>
2019-02-15 12:31:42 +0700

    commit

--- introduction-to-jest/kata/greeting-kata.js
+++ introduction-to-jest/kata/greeting-kata.js
@@ -1,14 +1,49 @@
 function greeting (person) {
-  if (isUpperCase(person)) {
-    return `HELLO ${person}!`
-  }
-  const name = person || 'my friend'
-  return `Hello, ${name}`
+	if (Array.isArray(person)) {
+		let name = ''
+		const cntPerson = person.length
+		if (cntPerson > 2) {
+			if (person.find(checkStr) !== undefined) {
+				let p = person.find(checkStr)
+				let index = person.indexOf(p)
+				person.splice(index, 1)
+				return `Hello, ${person.join(', ')} and HELLO ${p}!`
+			} else {
+				name = formatArray(person)
+				return `Hello, ${name}`
+			}
+		} else {
+			return `Hello, ${person[0]} and ${person[1]}`
+		}
+	} 
+	
+	if (isUpperCase(person)) {
+		return `HELLO ${person}!`
+	}
+	
+	const name = person || 'my friend'
+	return `Hello, ${name}`
 }
 
 function isUpperCase (word) {
-  if (!word) return false
-  return word.toUpperCase() === word
+	if (!word) return false
+	return word.toUpperCase() === word
+}
+
+function formatArray(arr){
+    var outStr = "";
+    if (arr.length === 1) {
+        outStr = arr[0];
+    } else if (arr.length === 2) {
+        outStr = arr.join(' and ');
+    } else if (arr.length > 2) {
+        outStr = arr.slice(0, -1).join(', ') + ' and ' + arr.slice(-1);
+    }
+    return outStr;
+}
+
+function checkStr(arr) {
+	if (isUpperCase(arr)) return arr
 }
 
 module.exports = {
--- introduction-to-jest/kata/greeting-kata.test.js
+++ introduction-to-jest/kata/greeting-kata.test.js
@@ -1,14 +1,28 @@
 const { greeting } = require('./greeting-kata')
 
 describe('greeting', () => {
-  it('given person name, should greeting person with hello', () => {
-    expect(greeting('Chris')).toEqual('Hello, Chris')
-  })
-  it('given null, should greeting friends', () => {
-    expect(greeting(null)).toEqual('Hello, my friend')
-  })
-  it(`given person name which upper case,
-      should greeting person with shouting tone`, () => {
-    expect(greeting('CHRIS')).toEqual('HELLO CHRIS!')
-  })
+	it('given person name, should greeting person with hello', () => {
+		expect(greeting('Chris')).toEqual('Hello, Chris')
+	})
+
+	it('given null, should greeting friends', () => {
+		expect(greeting(null)).toEqual('Hello, my friend')
+	})
+
+	it(`given person name which upper case,
+		should greeting person with shouting tone`, () => {
+		expect(greeting('CHRIS')).toEqual('HELLO CHRIS!')
+	})
+
+	it(`given 2 persons`, () => {
+		expect(greeting(["attapon","konger"])).toEqual('Hello, attapon and konger')
+	})
+
+	it(`given b`, () => {
+		expect(greeting(["Attapon","Konger","Pim"])).toEqual('Hello, Attapon, Konger and Pim')
+	})
+
+	it(`given c`, () => {
+		expect(greeting(["Attapon","KONGER","Pim"])).toEqual('Hello, Attapon, Pim and HELLO KONGER!')
+	})
 })
