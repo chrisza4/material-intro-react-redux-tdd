7c6240b6a97d08e419ddb090a9b574d32a42a7d8
Buri Paoton <bakeforce75@gmail.com>
2019-02-15 12:04:24 +0700

    pr 4 5 6

--- introduction-to-tdd/greeting-kata-step-1.js
+++ introduction-to-tdd/greeting-kata-step-1.js
@@ -2,13 +2,53 @@ function greeting (person) {
   if (isUpperCase(person)) {
     return `HELLO ${person}!`
   }
-  const name = person || 'my friend'
+  if(Array.isArray(person)){
+    var name = `Hello, `
+    var nameUppercase = `AND HELLO `
+    var namearr = [];
+    var nameUppercasearr = []; 
+    var checkUppercase = false;
+    for(let i=0; i < person.length; i++){
+      if(isUpperCase(person[i])){
+        nameUppercasearr.push(person[i])
+        checkUppercase = true;
+      }else{
+        namearr.push(person[i])
+      }
+    }
+
+    name += formatarrayNames(namearr);
+    nameUppercase += formatarrayNames(nameUppercasearr)
+    return !checkUppercase ? name : name+ ' ' +nameUppercase;
+  }
+  name = person || 'my friend'
   return `Hello, ${name}`
 }
 
 function isUpperCase (word) {
   if (!word) return false
+  if (Array.isArray(word)) return false
   return word.toUpperCase() === word
+}
+
+function formatarrayNames(person){
+  var text = '';
+  for(let i=0; i < person.length; i++){
+    if(isUpperCase(person[i])){
+      text += person[i]
+    }else{
+      if(i == person.length-1){
+        text += ' and '+person[i];
+      }
+      else if(i == person.length-2){
+        text += person[i];
+      }
+      else{
+        text += person[i]+', ';
+      }
+    }
+  }
+  return text;
 }
 
 module.exports = {
